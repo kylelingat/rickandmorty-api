6eee4cc47e300dad4996a48a48c89834e8be0804
Kyle Lingat <kylejlingat@gmail.com>
2018-09-17 09:26:20 -1000

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,16 +1,3 @@
-// var request = new XMLHttpRequest();
-// var data;
-// var i;
-// for (i = 0; i < 99; i++) {
-// }
-// request.open('GET', `https://rickandmortyapi.com/api/character/${i}`, true);
-//
-// request.onload = function () {
-//   console.log((request))
-//   // var data = JSON.parse(request.results)
-//   }
-//
-// request.send();
 
 
 
@@ -27,14 +14,6 @@ function apiCall(characterId) {
                   char.className = "charCard";
                   $("#charContainer").append(char)
 
-                  // var charNameNode = document.createElement("DIV");
-                  // var placeName = document.createElement("h1");
-                  // placeName.className = "heroTitle";
-                  // charNameNode.appendChild(placeName);
-                  // var nameApi = data.name;
-                  // var grabbedName = document.createTextNode(nameApi);
-                  // placeName.appendChild(grabbedName);
-                  // $(char).append(placeName);
 
                   var imgHolder = document.createElement("DIV");
                   imgHolder.className = "heroBackgroundImage";
